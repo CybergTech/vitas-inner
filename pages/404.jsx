7b925b29ba6c3287229ca975e36dbe095717336e
nickcarva <carvalhonick2002@gmail.com>
2020-09-24 14:41:06 -0300

    404 Page idea implemented

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 
 import TopHeader from '../components/TopHeader'
 import Header from '../components/Header'
+import DetailDots from '../components/DetailDots'
 import Footer from '../components/Footer'
 
-import grid from '../styles/grid/main.module.css'
+import grid from '../styles/grid/nobanners.module.css'
+import mainStyles from '../styles/main.module.css'
+import styles from '../styles/404.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function Custom404 () {
+function Home () {
   return (
     <div className={grid.wrapper}>
       <Head>
@@ -24,15 +29,42 @@ function Custom404 () {
       </header>
 
       <main className={grid.main}>
-        Página não encontrada<br />
-        Erro 404
+        <section className={mainStyles.contentSection}>
+          <div className={styles.container}>
+            <DetailDots style={{ right: '95%', bottom: '-27px' }} />
+
+            <div className={styles.subtitles}>
+              <h2 className={styles.title}>
+                A página que você está buscando<br/>
+                não foi encontrada :/
+              </h2>
+
+              <h4 className={styles.subtitle}>
+                Isso pode ter acontecido por que o conteúdo não está mais no ar ou você pode ter digitado o endereço errado.
+              </h4>
+
+              <button type="button" className={styles.button}>
+                <Link href="/">
+                  <a>Voltar à página principal</a>
+                </Link>
+              </button>
+            </div>
+
+            <div className={styles.iconContainer}>
+              <FontAwesomeIcon
+                icon="times-circle"
+                className={styles.icon}
+              />
+            </div>
+          </div>
+        </section>
       </main>
 
       <footer className={grid.footer}>
-        <Footer newsletter={false} maps={false} />
+        <Footer maps={false} newsletter={false} />
       </footer>
     </div>
   )
 }
 
-export default Custom404
+export default Home

07317705e195e910abd2bdf72a8e83d74524f910
nickcarva <carvalhonick2002@gmail.com>
2020-10-02 12:55:47 -0300

    Aside Cart Component implemented

--- components/Cart/index.jsx
+++ components/Cart/index.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 
+import ProductItem from '../ProductItem'
+
 import styles from './styles.module.css'
 
 function Cart () {
   return (
     <aside className={styles.container}>
+      <div className={styles.productsContainer}>
+        <h4 className={styles.subtitle}>MINHA SACOLA DE COMPRAS</h4>
 
+        <div className={styles.productsWrapper}>
+          <ProductItem asList showAddToCardButtons />
+          <ProductItem asList showAddToCardButtons />
+        </div>
+      </div>
+
+      <div className={styles.menuContainer}>
+        <div className={styles.options}>
+          <button
+            type="button"
+            className={styles.optionButton}
+          >
+            Aplicar cupom de desconto
+          </button>
+
+          <button
+            type="button"
+            className={styles.optionButton}
+          >
+            Limpar Carrinho
+          </button>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.price}>
+            <span className={styles.priceText}>Total:</span><br />
+            R$ 113,40
+          </div>
+        </div>
+      </div>
     </aside>
   )
 }
